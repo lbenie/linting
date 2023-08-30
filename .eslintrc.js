53311caab8d84a79b8863953b648b519b0642b48
Lucien Bénié <lucien.benie@gmail.com>
2023-08-30 19:57:55 -0400

    fix: add astro tsconfig reference

--- .eslintrc.js
+++ .eslintrc.js
@@ -87,6 +87,7 @@ module.exports = {
         'plugin:astro/recommended'
       ],
       parserOptions: {
+        project: ['./tsconfig.json'],
         extraFileExtensions: ['.astro'],
       },
       rules: {
